03a2aeca9b1881c36d5304554d44772f18043341
Jorge Gong <791487336@qq.com>
2023-02-20 15:55:27 +0800

    feat(config): add metro

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: gongxi33
+ * @Date: 2022-08-20 17:03:00
+ * @LastEditTime: 2023-02-20 15:53:55
+ * @LastEditors: gongxi33
+ * @Description:
+ * @FilePath: /rail-all/metro.config.js
+ */
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
@@ -5,4 +13,10 @@
  * @format
  */
 
-module.exports = {}
+const { mergeConfig } = require('metro-config')
+const { getMetroConfig } = require('@tarojs/rn-supporter')
+module.exports = mergeConfig({
+  // custom your metro config here
+  // https://facebook.github.io/metro/docs/configuration
+  resolver: {}
+}, getMetroConfig())
