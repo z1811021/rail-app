7a2777c09f3c72cbb0efdbd5a8ddd559fcc4590c
Jorge Gong <791487336@qq.com>
2023-02-20 14:23:33 +0800

    feat(test): playground code

--- android/app/src/main/java/com/tarodemo/MainActivity.java
+++ android/app/src/main/java/com/tarodemo/MainActivity.java
@@ -4,8 +4,25 @@ import expo.modules.ReactActivityDelegateWrapper;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.tarodemo.devmanager.TaroDevManager;
+
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(null);
+  }
+
+  @Override
+  public void finish() {
+    // clear ReactNativeHost on Activity finish
+    TaroDevManager.clearReactNativeHost();
+    super.finish();
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -18,11 +35,12 @@ public class MainActivity extends ReactActivity {
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-   * you can specify the rendered you wish to use (Fabric or the older renderer).
+   * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+   * (Paper).
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new MainActivityDelegate(this, getMainComponentName()));
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
@@ -36,6 +54,13 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+
+    @Override
+    protected boolean isConcurrentRootEnabled() {
+      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
   }
 }
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,23 @@
+/*
+ * @Author: gongxi33
+ * @Date: 2022-08-20 17:03:00
+ * @LastEditTime: 2023-02-20 14:06:43
+ * @LastEditors: gongxi33
+ * @Description:
+ * @FilePath: /rail-all/babel.config.js
+ */
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
   presets: [
-    ['taro', {
-      framework: 'react',
-      ts: false
-    }]
-  ]
-}
+    [
+      'taro',
+      {
+        framework: 'react',
+        ts: false,
+      },
+    ],
+    'module:metro-react-native-babel-preset',
+  ],
+  plugins: ['react-native-reanimated/plugin'],
+};
